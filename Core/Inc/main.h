f49e28ebd727ba4d283042e01ac9f752b0d32e7e
tsotnek <tsotne.karchava04@gmail.com>
2022-04-05 11:55:02 +0400

    Initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,133 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+//Bootloader Functional Prototypes
+#define VERIFY_CRC_SUCCESS 		0
+#define VERIFY_CRC_FAIL 			1
+
+
+//BL_VERSION is 1.0
+#define BL_VERSION 0x10
+
+
+void bootloader_uart_read_data(void);
+void bootloader_jump_to_user_app(void);
+
+void bootloader_handle_getver_cmd(uint8_t *bl_rx_buffer);
+void bootloader_handle_gethelp_cmd(uint8_t *pBuffer);
+void bootloader_handle_getcid_cmd(uint8_t *pBuffer);
+void bootloader_handle_getrdp_cmd(uint8_t *pBuffer);
+void bootloader_handle_go_cmd(uint8_t *pBuffer);
+void bootloader_handle_flash_erase_cmd(uint8_t *pBuffer);
+void bootloader_handle_mem_write_cmd(uint8_t *pBuffer);
+void bootloader_handle_en_rw_protect(uint8_t *pBuffer);
+void bootloader_handle_mem_read (uint8_t *pBuffer);
+void bootloader_handle_read_sector_protection_status(uint8_t *pBuffer);
+void bootloader_handle_read_otp(uint8_t *pBuffer);
+void bootloader_handle_dis_rw_protect(uint8_t *pBuffer);
+
+void bootloader_uart_write_data(uint8_t *pBuffer, uint32_t len);
+void bootloader_send_nack(void);
+void bootloader_send_ack(uint8_t command_code, uint8_t follow_len);
+
+uint8_t bootloader_verify_crc (uint8_t *pData, uint32_t len, uint32_t crc_host);
+uint8_t get_bootloader_version(void);
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+#define FLASH_SECTOR2_BASE_ADDRESS 0x08008000U
+
+#define	BL_GET_VER 							0x51
+#define	BL_GET_HELP 						0x52
+#define	BL_GET_CID 							0x53
+#define	BL_GET_RDP_STATUS 			0x54
+#define	BL_GO_TO_ADDR						0x55
+#define	BL_FLASH_ERASE					0x56
+#define	BL_MEM_WRITE			 			0x57
+#define	BL_EN_RW_PROTECT				0x58
+#define	BL_MEM_READ							0x59
+#define	BL_READ_SECTOR_P_STATUS	0x5A
+#define	BL_OTP_READ							0x5B
+#define	BL_DIS_R_W_PROTECT			0x5C
+
+
+/* ACK AND NACK bytes*/
+#define BL_ACK 		0xA5
+#define BL_NACK	  0x7F
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ MDK-ARM/Bootloader_NUCLEO_STM32F446RE/startup_stm32f446xx.d
@@ -0,0 +1,1 @@
+bootloader_nucleo_stm32f446re\startup_stm32f446xx.o: startup_stm32f446xx.s
